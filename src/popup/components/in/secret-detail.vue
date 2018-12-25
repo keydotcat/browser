9529a1f25bd91afb9b63d80df2b0fcab7e0a557e
Adria Casajus <adriancasajus@gmail.com>
2018-12-25 22:52:53 +0100

    fix: Style in the search tab

--- src/popup/components/in/secret-detail.vue
+++ src/popup/components/in/secret-detail.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-left pl-1">
-    <div class="row">
-      <div class="col font-weight-bold d-flex" @click="expanded=!expanded">
+    <div class="row m-0">
+      <div class="col font-weight-bold d-flex p-0" @click="expanded=!expanded">
         <i v-if="!expanded" class="material-icons mt-auto mb-auto">chevron_right</i>
         <i v-if="expanded" class="material-icons mt-auto mb-auto">expand_more</i>
         <p class="m-0">{{secret.data.name}}</p>
@@ -9,7 +9,7 @@
     </div>
     <div class="pl-2" v-if="expanded">
       <p class="text-muted m-1">Credentials</p>
-      <div class="row pl-3" v-for="cred in secret.data.creds">
+      <div class="row m-0 pl-3" v-for="cred in secret.data.creds">
         <div class="col-1 pointme" @click="fillWithCred(cred)">
           <i class="material-icons mt-auto mb-auto">open_in_browser</i>
         </div>
@@ -18,7 +18,7 @@
         </div>
       </div>
       <p v-if="secret.data.urls.length > 0" class="text-muted m-1">URLs</p>
-      <div class="row pl-3" v-for="url in secret.data.urls">
+      <div class="row m-0 pl-3" v-for="url in secret.data.urls">
         <div class="col-1 text-right">
           <a class="text-muted" :href="url" target="_blank"><i class="material-icons mt-auto mb-auto">open_in_new</i> </a>
         </div>
